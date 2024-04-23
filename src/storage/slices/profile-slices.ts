b25fa18c4c9837e9c3b303132fb49fdff6bf5459
evikary <daria_more10@mail.ru>
2024-04-23 21:02:04 +0300

    feat: add redux and reduxDevTools and add store

---
+++ src/storage/slices/profile-slices.ts
@@ -0,0 +1,18 @@
+// import { createSlice } from '@reduxjs/toolkit';
+// import { RequestStatus } from '../types/type-store';
+
+// const initialState = {
+// 	user: null,
+// 	status: RequestStatus.Idle,
+// 	failed: null,
+// };
+
+// export const userSlice = createSlice({
+// 	name: 'user',
+// 	initialState,
+//     reducers: {
+
+//     },
+
+//     selectors:
+// });
